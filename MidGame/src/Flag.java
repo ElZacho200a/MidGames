7d01e69dfc413f24905b0fa1515652ae7d5a6273
ElZacho200a <75814263+ElZacho200a@users.noreply.github.com>
2023-02-02 15:36:01 +0100

    Update Flag.java
    sfs

--- MidGame/src/Flag.java
+++ MidGame/src/Flag.java
@@ -2,7 +2,7 @@
 public class Flag extends Sprite{
 	
 	public Flag() {
-		super(new SpriteImages(5, 5,"Images\\Flag.png" ) , 0,0);
+		super(new SpriteImages(5, 5,"Images\\Flag.png" ) , 0,0); // yo
 	}
 
 }
